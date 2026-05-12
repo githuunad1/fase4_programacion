104d23fa8108a006d4120829f7673f95aab1a51c
githuunad1 <canva_micorreo@hotmail.com>
2026-05-12 17:27:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 # Fase 4 - Programacion
 # Sistema Integral de Gestion de Clientes, Servicios y Reservas
 # Empresa: Software FJ
-# Autor: [Escriba aqui su nombre]
-# Grupo: [Escriba aqui su grupo]
+# Autor: GABRIELA BARON ESPRIELLA
+# Grupo: 213023_10
 # Curso: Programacion - UNAD
 # ============================================================
 
